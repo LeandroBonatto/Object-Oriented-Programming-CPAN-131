02032ab1af5ae1fc6ae9f8fc565781ef277b8aa5
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-16 12:00:57 -0400

    Lab1-CelsiusToF

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author LEANDRINHO
+ */
+public class CelsiusToFahrenheit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int celsius = 20;
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        
+        System.out.println("Temperature in Celsius: " + celsius + "\n" + 
+                "After conversion" + "\n" + 
+                "Temperature in Fahrenheit: " + fahrenheit );
+        
+        
+    }
+    
+}
