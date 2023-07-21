dca7bef413de93d78dbb7352f725a138fbe1fa8e
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-21 10:21:12 -0400

    Week-11-BankAccountPolymorphismStart

---
+++ Week11-BankAccountPolymorphism-Start/src/BankClasses/MyConstants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankClasses;
+
+/**
+ *
+ * @author Amrit
+ */
+public class MyConstants {
+    public static final double INTEREST_RATE = 0.02;
+    public static final double MIN_BALANCE = 100;
+    public static final int FREE_TRANSACTIONS = 3;
+    public static final double TRANSACTION_FEE = 0.50;
+}
